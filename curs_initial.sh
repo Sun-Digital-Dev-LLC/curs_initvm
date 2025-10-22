150fd59b6273a810734594fce53304fe0f5b7538
MrBandi_tw <qwea6935@gmail.com>
2025-10-23 01:09:52 +0800

    Add initial script for system initialization and setup

---
+++ curs_initial.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "================================"
+echo "主機初始化..."
+echo "================================"
+
+# 更新系统
+echo "正在更新系统套件..."
+sudo apt update && sudo apt upgrade -y
+echo "系统套件更新完成。"
+
+# 安装 qemu-guest-agent
+echo "正在安装 Proxmox Guest Agent..."
+sudo apt install -y qemu-guest-agent
+
+echo "檢查是否有 snap 套件..."
+if ! command -v snap &> /dev/null; then
+    echo "未找到 snap 套件，正在安裝..."
+    sudo apt install -y snapd
+else
+    echo "snap 套件已安裝。"
+fi
+echo "正在安裝 btop 監控工具..."
+sudo snap install btop
+echo "btop 安裝完成。"
+
+# 安装其他常用套件
+echo "正在安装其他常用套件..."
+sudo apt install -y \
+    net-tools \
+
+# 启动 qemu-guest-agent 服务
+echo "啟用 qemu-guest-agent 中..."
+sudo systemctl enable --now qemu-guest-agent
+
+echo "設定 ufw 防火牆中..."
+sudo ufw allow 22
+sudo ufw enable
+
+# 清理不需要的套件
+echo "清理系统..."
+sudo apt autoremove -y
+sudo apt autoclean
+
+echo "================================"
+echo "系统初始化完成！"
+echo "================================"
